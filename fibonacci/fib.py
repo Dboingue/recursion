bceb9d0d9841abb64784fa9b89c5364f396ecd96
jomega-github <64698816+jomega-github@users.noreply.github.com>
2021-04-10 14:33:45 -0500

    New file.

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -105,11 +105,23 @@ def sanity_check_args(n):
     # NB: Dispite the default recursion limit being 1000, we may
     # not be able to do that many. However, for fib() it does not matter
     # because we will never get near the limit.
-    # TODO: Warn about big n!
-    max_n = 999
+
+    # The first 40 Fib(n) values.
+    fib_val = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+               987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368,
+               75025, 121393, 196418, 317811, 514229, 832040, 1346269,
+               2178309, 3524578, 5702887, 9227465, 14930352, 24157817,
+               39088169, 63245986, 102334155]
+
+    max_n = len(fib_val)-2
     if n > max_n:
         print("error: n must be less than {}".format(max_n))
         sys.exit(1)
+    if n > 30:
+        print("**********")
+        print("warning: The value of Fib({})".format(n),
+              "will take {} recursive calls!".format(2*fib_val[n+1]-1))
+        print("**********\n")
 
 
 def run_with_args(args):
