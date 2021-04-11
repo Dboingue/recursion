f70fda5f5d215127aa9adc925fcf517ac22e6cf5
jomega-github <64698816+jomega-github@users.noreply.github.com>
2021-04-11 09:23:29 -0500

    Python script to create sample_output.txt.

---
+++ simpletree/create_sample_output.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+"""Create sample output file."""
+
+import subprocess
+import os
+
+try:
+    os.remove("sample_output.txt")
+except OSError:
+    pass
+
+with open("sample_output.txt", 'a+') as sample:
+    print(">python simpletree.py -h", file=sample, flush=True)
+    subprocess.run(["python", "simpletree.py", "-h"],
+                   shell=True,
+                   stdout=sample,
+                   stderr=subprocess.STDOUT)
+    print(file=sample)
+
+    print(">python simpletree.py a", file=sample, flush=True)
+    subprocess.run(["python", "simpletree.py", "a"],
+                   shell=True,
+                   stdout=sample,
+                   stderr=subprocess.STDOUT)
+    print(file=sample)
+
+    print(">python simpletree.py -1", file=sample, flush=True)
+    subprocess.run(["python", "simpletree.py", "-1"],
+                   shell=True,
+                   stdout=sample,
+                   stderr=subprocess.STDOUT)
+    print(file=sample)
+
+    print(">python simpletree.py --max 20 30", file=sample, flush=True)
+    subprocess.run(["python", "simpletree.py", "--max", "20", "30"],
+                   shell=True,
+                   stdout=sample,
+                   stderr=subprocess.STDOUT)
+    print(file=sample)
+
+    print(">python simpletree.py --max 30 10", file=sample, flush=True)
+    subprocess.run(["python", "simpletree.py", "--max", "30", "10"],
+                   shell=True,
+                   stdout=sample,
+                   stderr=subprocess.STDOUT)
+    print(file=sample)
+
+    print(">python simpletree.py --max 100 20", file=sample, flush=True)
+    subprocess.run(["python", "simpletree.py", "--max", "100", "20"],
+                   shell=True,
+                   stdout=sample,
+                   stderr=subprocess.STDOUT)
